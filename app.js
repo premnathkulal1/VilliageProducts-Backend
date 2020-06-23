fc84061c524d7d6300763e40841f3592660d7943
premnathkulal <premnathkulal1998@gmail.com>
2020-06-23 20:24:36 +0530

    Setup for Inserting and Serving for products Updated

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ connect.then((db) => {
 
 var app = express();
 
-// Secure traffic only
+/*Secure traffic only
 app.all('*', (req, res, next) => {
   if (req.secure) {
     return next();
@@ -28,7 +28,7 @@ app.all('*', (req, res, next) => {
   else {
     res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
   }
-});
+});*/
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
